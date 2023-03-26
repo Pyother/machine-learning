1a89ec14e762a1774c10eb86d4986c5236fe4bd7
piotr <piotr.dominik.sobol@gmail.com>
2023-03-26 18:54:29 +0200

    Save data from Otomoto into dataset and csv file

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from bs4 import BeautifulSoup
-from .model import Volkswagen
+from model import Volkswagen
+import pandas as pd
 import requests
 
-for page_number in range(1, 3):
+data = []
+
+for page_number in range(1, 5):
     # There is 563 pages. 
 
     url = f"https://www.otomoto.pl/osobowe/volkswagen?page={page_number}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    prices = soup.find_all('span', attrs={'class': 'ooa-1bmnxg7 eayvfn611'})
+    articles = soup.find_all('article', attrs={'class': 'ooa-1g2kumr eayvfn60'})
 
-    for price in prices:
-        print(price.text)
+    for article in articles:
+        
+        price = article.find_all('span', attrs={'class': 'ooa-1bmnxg7 eayvfn611'})
+        rest = article.find_all('li', attrs={'class': 'ooa-1k7nwcr e19ivbs0'})
+        if(rest[0].text == 'Niski przebieg'): row = Volkswagen(price[0].text, rest[1].text, rest[2].text, rest[3].text)
+        else : row = Volkswagen(price[0].text, rest[0].text, rest[1].text, rest[2].text)
+        row.clean_data()
+        data.append(row.return_data())  
+
+df = pd.DataFrame(data, columns=['Price', 'Year', 'Mileage', 'Tank capacity'])
+print(df)
+
+df.to_csv('otomoto.csv')
+
+    
+
+
+
+
+        
+
+    
+
+    
+        
--- model.py
+++ model.py
@@ -1,9 +1,37 @@
-class Volkswagen():
+class Volkswagen(object):
 
     def __init__(self, price, year, mileage, fuel):
         self.price = price
         self.year = year
         self.mileage = mileage
         self.fuel = fuel
+
+    def clean_data(self):
+
+        # Price
+        text = self.price.replace(" ", "")
+        text = text.replace("PLN", "")
+        self.price = int(text)
+
+        # Year
+        self.year = int(self.year)
+
+        # Mileage
+        text = self.mileage.replace(" ", "")
+        text = text.replace("km", "")
+        self.mileage = int(text)
+
+        # Fuel
+        text = self.fuel.replace(" ", "")
+        text = text.replace("cm3", "")
+        self.fuel = int(text)
+
+    def return_data(self):
+
+        return ([self.price, self.year, self.mileage, self.fuel])
+
+
+
+        
 
     
